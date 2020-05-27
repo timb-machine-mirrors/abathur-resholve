bed223c174ccaf8e267d52f27e37e1257eb70c02
Travis A. Everett <travis.a.everett@gmail.com>
2020-05-27 11:23:09 -0500

    debug for nixos

--- tests/nix/future_perfect_tense/conjure.sh
+++ tests/nix/future_perfect_tense/conjure.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+set -x
 test_future_perfection() {
     echo "nothing up my sleeve"
     {
-        set -x
+        # set -x
         type jq openssl
-        set +x
+        # set +x
     } && true
 }
 
